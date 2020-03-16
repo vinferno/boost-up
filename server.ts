731f4b5d48fde095b6e27d0b3c91ff4fb146351f
Vinson Fernandez <vinson.fernandez@admindirect.com>
2020-03-16 15:08:43 -0700

    add vendor

--- server.ts
+++ server.ts
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT ? process.env.PORT : 5000;
 
-
-
 app.use(express.static('public'));
+app.use(express.static('vendor'));
 app.get('/', (req, res) => {
     res.sendFile( __dirname + '/landing/index.html');
 });
+
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
